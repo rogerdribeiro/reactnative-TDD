508c716d73f567a338c40f0efb4f712a30bddd0e
Rogerd Ribeiro <rogerd.ribeiro@aluno.ufop.edu.br>
2019-02-27 20:22:31 -0300

    testando api nativa

--- __tests__/components/List.test.js
+++ __tests__/components/List.test.js
@@ -1,6 +1,7 @@
 import React from "react";
-import { Text, FlatList, Button } from "react-native";
+import { Text, FlatList, Button, AsyncStorage } from "react-native";
 import { shallow } from "enzyme";
+import sinon from "sinon";
 
 import List from "../../src/components/List";
 
@@ -41,7 +42,7 @@ describe("Testing List", () => {
 
   it("should add new post", () => {
     const wrapper = shallow(<List />);
-    wrapper.find(Button).simulate("press");
+    wrapper.find({ id: "new" }).simulate("press");
     expect(wrapper.state("posts")).toHaveLength(1);
   });
 
@@ -51,5 +52,23 @@ describe("Testing List", () => {
     wrapper.instance().deletePost(1);
 
     expect(wrapper.state("posts")).toEqual(posts.filter(post => post.id !== 1));
+  });
+
+  it("should save posts", () => {
+    sinon.spy(AsyncStorage, "setItem");
+    const wrapper = shallow(<List />);
+    wrapper.setState({ posts });
+    wrapper.find({ id: "save" }).simulate("press");
+
+    expect(AsyncStorage.setItem.calledOnce).toBe(true);
+    expect(AsyncStorage.setItem.args[0][1]).toBe(JSON.stringify(posts));
+  });
+
+  it("should load post on init", () => {
+    sinon.stub(AsyncStorage, "getItem").returns(JSON.stringify(posts));
+    const wrapper = shallow(<List />);
+
+    expect(AsyncStorage.getItem.calledOnce).toBe(true);
+    expect(AsyncStorage.getItem.returnValues[0]).toBe(JSON.stringify(posts));
   });
 });
--- src/components/List/index.js
+++ src/components/List/index.js
@@ -1,11 +1,17 @@
 import React, { Component } from "react";
-import { View, Text, FlatList, Button } from "react-native";
+import { View, Text, FlatList, Button, AsyncStorage } from "react-native";
 import Post from "../Post";
 
 export default class List extends Component {
   state = {
     posts: []
   };
+  async componentDidMount() {
+    const posts = JSON.parse(
+      (await AsyncStorage.getItem("@TestRN:posts")) || []
+    );
+    this.setState({ posts });
+  }
 
   renderPosts = () => (
     <FlatList
@@ -23,13 +29,20 @@ export default class List extends Component {
   deletePost = id => {
     this.setState({ posts: this.state.posts.filter(post => post.id !== id) });
   };
+  savePosts = async () => {
+    await AsyncStorage.setItem(
+      "@TestRN:posts",
+      JSON.stringify(this.state.posts)
+    );
+  };
 
   render() {
     const { posts } = this.state;
     return (
       <View>
         {posts.length > 0 ? this.renderPosts() : <Text>Nenhum post</Text>}
-        <Button title="Add post" onPress={this.addPost} />
+        <Button id="new" title="Add post" onPress={this.addPost} />
+        <Button id="save" title="save post" onPress={this.savePosts} />
       </View>
     );
   }
